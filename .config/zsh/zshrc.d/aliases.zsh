e1fdc3c71916fb1cb63382cd178bd6e1e41718b1
moposx <moposx@mopo.sx>
2026-05-29 20:22:08 +0800

    Refine command aliasing

--- .config/zsh/zshrc.d/aliases.zsh
+++ .config/zsh/zshrc.d/aliases.zsh
@@ -1,12 +1,18 @@
-alias cp='cp -iv'
-alias ls='ls --color=always'
-alias l="eza"
-alias la="eza -a"
-alias lal="eza -lah"
-alias ll="eza -lh"
-alias lla="lal"
-alias man="batman"
-alias md="mkdir -pv"
-alias mkdir="mkdir -pv"
-alias mv='mv -iv'
-alias rm='rm -iv'
+alias cp='command cp -iv'
+alias ls='command ls --color=always'
+alias mv='command mv -iv'
+alias rm='command rm -iv'
+alias md='command mkdir -pv'
+alias mkdir='command mkdir -pv'
+
+if command -v eza &> /dev/null; then
+    alias ls="eza --icons --color --git --group-directories-first --time-style=long-iso"
+    alias l='ls'
+    alias ll="ls -lgh"
+    alias la="ls -lagh"
+    alias lla="la"
+    alias lt="ls --tree --level=2"
+    alias ltt="ls --tree --level=3"
+    alias lm="ls -lagh --sort=modified --reverse"
+    alias lS="ls -lagh --sort=size --reverse"
+fi
